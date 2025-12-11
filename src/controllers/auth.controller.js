c531ec0a46e0e50bb432afa944b6c9b6056cd185
fatchurrosii <fatchurr0198@gmail.com>
2025-12-11 14:49:38 +0700

    feat: add max user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -14,6 +14,12 @@ exports.register = async (req, res) => {
     if(existingUser){
       return responseUtils.BadRequestResponse(res, 'Username already exists')
     }    
+    
+    const countUser = await db.User.count();
+    
+    if(countUser == 3){
+      return responseUtils.ForbiddenResponse(res, 'Maximum user limit reached');
+    }
 
     const user = await db.User.create({ username,  password });
     const token = jwtService.sign({ id: user.id, username: user.username });
@@ -73,23 +79,6 @@ exports.logout = async (req, res) => {
     return responseUtils.SuccessResponse(res,'User Logout successfully');
   }catch(err){
     console.error("[Logout] Error", err);
-    return responseUtils.InternalServerErrorResponse(res, err.message);
-  }
-}
-
-exports.getUserPaginated() = async (req, res) => {
-  
-  const { page = 1, limit = 10 } = req.query;
-  const offset = (page - 1) * limit;
-  
-  try{
-    
-    const users = await db.User.findAll({ offset, limit });
-    
-    return responseUtils.SuccessResponse(res,'Users Paginated successfully', users);
-    
-  }catch(err){
-    console.error("[getUserPaginated] Error", err);
     return responseUtils.InternalServerErrorResponse(res, err.message);
   }
 }
